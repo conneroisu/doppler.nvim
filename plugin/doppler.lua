34ffec2bf87e4e911fb801a863e0f5b90ba801b7
connero <88785126+conneroisu@users.noreply.github.com>
2024-05-02 14:54:12 -0500

    update to be more async friendly

--- plugin/doppler.lua
+++ plugin/doppler.lua
@@ -1,5 +1,44 @@
 vim.api.nvim_create_user_command("DopplerConfigs", require("doppler").doppler_configs, {})
 
-vim.cmd("silent !set -a")
-vim.cmd("silent !source <(doppler secrets download --no-file --format env)")
-vim.cmd("silent !set +a")
+local Job = require'plenary.job'
+
+-- Define a function to chain jobs sequentially
+local function chain_jobs(jobs)
+  local i = 1
+  local function run_next_job()
+    if i <= #jobs then
+      jobs[i]:start()
+      i = i + 1
+    else
+      -- Optional: callback when all jobs are completed
+      print("All commands completed")
+    end
+  end
+
+  for _, job in ipairs(jobs) do
+    job.on_exit = function()
+      run_next_job()
+    end
+  end
+
+  run_next_job()
+end
+
+-- Create jobs for each command
+local set_a = Job:new({
+  command = 'bash',
+  args = {'-c', 'set -a'}
+})
+
+local source_doppler = Job:new({
+  command = 'bash',
+  args = {'-c', 'source <(doppler secrets download --no-file --format env)'}
+})
+
+local set_plus_a = Job:new({
+  command = 'bash',
+  args = {'-c', 'set +a'}
+})
+
+-- Chain the jobs to ensure they are run in sequence
+chain_jobs({set_a, source_doppler, set_plus_a})
